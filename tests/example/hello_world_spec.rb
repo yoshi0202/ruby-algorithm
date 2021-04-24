a7eee126eabeec21248281b20cb621290d5cc204
Yoshiki Kobayashi <kobayashi020202@gmail.com>
2021-04-24 17:09:04 +0900

    fix: 失敗時のサンプルをコメントアウト

--- tests/example/hello_world_spec.rb
+++ tests/example/hello_world_spec.rb
@@ -2,7 +2,8 @@ require_relative '../../questions/example/hello_world'
 
 describe HelloWorld do
   it "return Hello,World" do
-    expect(HelloWorld.hello).to eq 'Hello, Wold!'
     expect(HelloWorld.hello).to eq 'Hello, World!'
+    # Failue example
+    # expect(HelloWorld.hello).to eq 'Hello, Word!'
   end
 end
